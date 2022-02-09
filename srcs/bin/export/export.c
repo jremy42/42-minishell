56aa0380f747311c1c20300338e4bc60351431ee
Frederic LE BLAY <fle-blay@e2r1p14.clusters.42paris.fr>
2022-02-09 11:56:17 +0100

    WIP env

--- srcs/bin/export/export.c
+++ srcs/bin/export/export.c
@@ -6,21 +6,17 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 10:21:11 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/02/09 10:36:16 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/02/09 11:53:51 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-int	__export(t_msh *msh, char *key_val)
+static int	add_key_val(t_msh *msh, char *key_val, int i)
 {
 	char	***new_env;
-	int		i;
 
-	i = 0;
-	while (msh->envp[i])
-		i++;	
 	new_env = (char ***)malloc((i + 2) * sizeof(char **));
 	if (!new_env)
 		return (__FAIL);
@@ -34,8 +30,53 @@ int	__export(t_msh *msh, char *key_val)
 	new_env[i][0] = __strdup(key_val);
 	new_env[i][1] = __strdup("1");
 	new_env[i][2] = NULL;
-	while (--i)
+	while (--i > -1)
 		new_env[i] = msh->envp[i];
+	free(msh->envp);
 	msh->envp = new_env;
 	return (__SUCCESS);
+}
+
+static int	modify_status_key_val(t_msh *msh, char *key)
+{
+	int		i;
+
+	i = 0;
+	while (msh->envp[i])
+	{
+		if (!__strncmp(msh->envp[i][0], key, __strlen(key)))
+			break ;
+		i++;
+	}
+	if (!msh->envp[i])
+		return (__FAIL);
+	free(msh->envp[i][1]);
+	msh->envp[i][1] = __strdup("1");
+	if (!msh->envp[i][1])
+		return (__FAIL);
+	return (__SUCCESS);
+}
+
+int	__export(t_msh *msh, char *key_val)
+{
+	int		i;
+
+	i = 0;
+	while (msh->envp[i])
+		i++;	
+	printf("i : >%d<\n", i);
+	if (__strchr(key_val, '='))
+	{
+		if (add_key_val(msh, key_val, i) == __SUCCESS)
+			return (__SUCCESS);
+		else
+			return (__FAIL);
+	}
+	else
+	{
+		if (modify_status_key_val(msh, key_val) == __SUCCESS)
+			return (__SUCCESS);
+		else
+			return (__FAIL);
+	}
 }
