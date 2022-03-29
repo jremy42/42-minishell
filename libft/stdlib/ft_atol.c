3fb6cfb9cb8863b420a7abbb3b14eba9154c8383
Fred <frederic.leblay@gmail.com>
2022-03-29 18:15:31 +0200

    wip tester 2

--- libft/stdlib/ft_atol.c
+++ libft/stdlib/ft_atol.c
@@ -6,16 +6,21 @@
 /*   By: jremy <jremy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:53:33 by jremy             #+#    #+#             */
-/*   Updated: 2022/03/29 17:50:46 by jremy            ###   ########.fr       */
+/*   Updated: 2022/03/29 18:11:29 by fred             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	__check_overflow(long long result)
+static	int	__check_overflow(unsigned long long result, int sign)
 {
-	if (result < LLONG_MIN || result > LLONG_MAX)
-		return (-1);
+	unsigned long long min;
+
+	min = 9223372036854775807;
+	if (result > min && sign == -1)
+		return (1);
+	if (result > 9223372036854775807 && sign == 1)
+		return (1);
 	return (0);
 }
 
@@ -46,7 +51,7 @@ int	__atol(const char *nb, int *res)
 	while (__isdigit(nb[i]) && nb[i])
 	{
 		result = result * 10 + (nb[i] - '0');
-		if (__check_overflow(result * sign) == -1)
+		if (__check_overflow(result, sign))
 			return (0);
 		i++;
 	}
