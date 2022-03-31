14af75ca9a7fd4d07a1ff706be0dc6bd87afc18d
Frederic LE BLAY <fle-blay@e2r2p8.clusters.42paris.fr>
2022-03-31 12:04:38 +0200

    Norme last_globe

--- srcs/expand/last_globe.c
+++ srcs/expand/last_globe.c
@@ -6,7 +6,7 @@
 /*   By: jremy <jremy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 09:52:39 by jremy             #+#    #+#             */
-/*   Updated: 2022/03/30 16:35:24 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/03/31 12:04:02 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,14 @@ t_glob	*__glob_new(char *content, t_globe_type type, int size)
 
 int	__move_to_next_unquoted_charset(char *str, char *charset)
 {
-	t_state s_status;
-	t_state q_status;
-	int i;
+	t_state	s_status;
+	t_state	q_status;
+	int		i;
 
 	q_status = UNQUOTE;
 	s_status = 0;
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == '\\' && str[i + 1] && __need_to_escape(i, q_status, str)
 			&& !s_status)
@@ -48,8 +48,9 @@ int	__move_to_next_unquoted_charset(char *str, char *charset)
 			continue ;
 		}
 		q_status = __return_state(str[i], q_status, s_status);
-		if(!s_status && q_status == UNQUOTE && __is_in_charset(str[i], charset))
-			return(i);
+		if (!s_status && q_status == UNQUOTE
+			&& __is_in_charset(str[i], charset))
+			return (i);
 		else
 			i++;
 		s_status = 0;
@@ -59,14 +60,14 @@ int	__move_to_next_unquoted_charset(char *str, char *charset)
 
 int	__move_to_next_unquoted_char(char *str, char c)
 {
-	t_state s_status;
-	t_state q_status;
-	int i;
+	t_state	s_status;
+	t_state	q_status;
+	int		i;
 
 	q_status = UNQUOTE;
 	s_status = 0;
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == '\\' && str[i + 1] && __need_to_escape(i, q_status, str)
 			&& !s_status)
@@ -76,8 +77,8 @@ int	__move_to_next_unquoted_char(char *str, char c)
 			continue ;
 		}
 		q_status = __return_state(str[i], q_status, s_status);
-		if(!s_status && str[i] == c && q_status == UNQUOTE )
-			return(i);
+		if (!s_status && str[i] == c && q_status == UNQUOTE)
+			return (i);
 		else
 			i++;
 		s_status = 0;
@@ -99,6 +100,7 @@ void	__globadd_back(t_glob **alst, t_glob *new)
 		nextlst->next = new;
 	}
 }
+
 void	__glob_list_clear(t_glob *start)
 {
 	t_glob	*next_to_free;
@@ -114,62 +116,53 @@ void	__glob_list_clear(t_glob *start)
 
 int	__copy_dir_entries(t_list **dir_content, struct dirent *curr_dir, DIR *dp)
 {
-    char    *tmp;
-    t_list  *new_entry;
+	char	*tmp;
+	t_list	*new_entry;
 
-    while (curr_dir)
-    {
-        if(curr_dir->d_name[0] != '.')
-        {
-            tmp = __strdup(curr_dir->d_name);
-            if (!tmp)
-                return (__lstclear(dir_content, free), closedir(dp), 0);
-            new_entry = __lstnew(tmp);
-            if (!new_entry)
-                return (free(tmp), __lstclear(dir_content, free),
+	while (curr_dir)
+	{
+		if (curr_dir->d_name[0] != '.')
+		{
+			tmp = __strdup(curr_dir->d_name);
+			if (!tmp)
+				return (__lstclear(dir_content, free), closedir(dp), 0);
+			new_entry = __lstnew(tmp);
+			if (!new_entry)
+				return (free(tmp), __lstclear(dir_content, free),
 					closedir(dp), 0);
-            __lstadd_back(dir_content, new_entry);
-        }
-        curr_dir = readdir(dp);
-    }
+			__lstadd_back(dir_content, new_entry);
+		}
+		curr_dir = readdir(dp);
+	}
 	return (1);
 }
 
-int   __init_dir_content(t_list **dir_content)
+int	__init_dir_content(t_list **dir_content)
 {
-    char    path[PATH_MAX];
-    struct  dirent *curr_dir;
-    DIR     *dp;
+	char			path[PATH_MAX];
+	struct dirent	*curr_dir;
+	DIR				*dp;
 
-    if (!getcwd(path, PATH_MAX))
-        return (__putendl_fd("Minishell : getcwd: cannot access directories:",2)
+	if (!getcwd(path, PATH_MAX))
+		return (__putendl_fd("Minishell : getcwd:\
+		cannot access directories:", 2)
 			, 1);
-    dp = opendir(path);
-    if (!dp)
-        return (1);
-    curr_dir = readdir(dp);
+	dp = opendir(path);
+	if (!dp)
+		return (1);
+	curr_dir = readdir(dp);
 	if (!__copy_dir_entries(dir_content, curr_dir, dp))
 		return (0);
-    if (closedir(dp) < 0)
-        return (__lstclear(dir_content, free), 0);
-    return (1);
+	if (closedir(dp) < 0)
+		return (__lstclear(dir_content, free), 0);
+	return (1);
 }
-
-void    __print_dir(t_list *dir_content)
-{
-    while (dir_content)
-    {
-        dir_content = dir_content->next;
-    }
-}
-
-
 
 int	__get_char_quote_status(char *str, char *to_find)
 {
-	t_state s_status;
-	t_state q_status;
-	int i;
+	t_state	s_status;
+	t_state	q_status;
+	int		i;
 
 	q_status = UNQUOTE;
 	s_status = 0;
@@ -190,38 +183,36 @@ int	__get_char_quote_status(char *str, char *to_find)
 	if (__strchr("\\'\"", str[i]) && q_status != UNQUOTE)
 		q_status = __return_state(str[i], q_status, s_status);
 	if (!s_status && q_status == UNQUOTE)
-			return(0);
+		return (0);
 	return (1);
 }
 
-
-t_globe_type __get_next_state(t_globe_type current_state, char *token)
+t_globe_type	__get_next_state(t_globe_type current_state, char *token)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	
-	while(token[i] && token[i] == '*'
+	while (token[i] && token[i] == '*'
 		&& !__get_char_quote_status(token, &token[i]))
 		i++;
 	if (__move_to_next_unquoted_char(token + i, '*') < 0)
 		return (LAST);
 	if (current_state == FIRST)
-		return (MIDDLE);	
+		return (MIDDLE);
 	return (MIDDLE);
 }
 
-int __add_new_glob(char *to_glob_expand,
+int	__add_new_glob(char *to_glob_expand,
 t_globe_type *state, t_glob **glob, int j)
 {
 	t_glob	*new;
-	
+
 	if (j == 0)
 		new = __glob_new(NULL, *state, 0);
 	else
 		new = __glob_new(to_glob_expand - j, *state, j);
 	if (!new)
-		return (__glob_list_clear(*glob),0);
+		return (__glob_list_clear(*glob), 0);
 	__globadd_back(glob, new);
 	*state = __get_next_state(*state, to_glob_expand);
 	return (1);
@@ -240,49 +231,48 @@ void	__get_glob_size(int glob_len, int *i, int *j, char **tge)
 	}
 }
 
-
-int __add_remaining_globs(char **tge, t_glob **glob, t_globe_type state)
+int	__add_remaining_globs(char **tge, t_glob **glob, t_globe_type state)
 {
-	int i;
-	int j;
-	int glob_len;
-	
+	int	i;
+	int	j;
+	int	glob_len;
+
 	i = 0;
 	j = 0;
 	glob_len = __strlen(*tge);
 	while (i < glob_len)
-    {
+	{
 		if ((*tge)[i] == '*' && !__get_char_quote_status(*tge, &(*tge)[i]))
 		{
 			__get_glob_size(glob_len, &i, &j, tge);
-			if(!__add_new_glob((*tge) + i, &state, glob, j))
+			if (!__add_new_glob((*tge) + i, &state, glob, j))
 				return (0);
 			j = 0;
 		}
 		else
 			i++;
-    }
+	}
 	return (1);
 }
 
-t_glob *__create_glob_lst(char **to_glob_expand)
+t_glob	*__create_glob_lst(char **to_glob_expand)
 {
-    t_glob  *glob;
+	t_glob			*glob;
 	t_globe_type	state;
 	int				i;
- 
-    glob = NULL;
+
+	glob = NULL;
 	state = FIRST;
 	i = 0;
 	if ((*to_glob_expand)[0] == '*')
 	{
-		if(!__add_new_glob(*to_glob_expand, &state, &glob, 0))
+		if (!__add_new_glob(*to_glob_expand, &state, &glob, 0))
 			return (NULL);
 	}
 	else
 	{
 		i = __move_to_next_unquoted_char(*to_glob_expand, '*');
-		if(!__add_new_glob((*to_glob_expand) + i, &state, &glob, i))
+		if (!__add_new_glob((*to_glob_expand) + i, &state, &glob, i))
 			return (NULL);
 	}
 	if (!__add_remaining_globs(to_glob_expand, &glob, state))
@@ -292,26 +282,27 @@ t_glob *__create_glob_lst(char **to_glob_expand)
 
 int	__find_end(char *file_name, t_glob *glob_lst)
 {
-	char *tmp;
+	char	*tmp;
 
 	tmp = __strrstr(file_name, glob_lst->to_find);
 	if (tmp && tmp[__strlen(glob_lst->to_find)] == 0)
-		return(1);	
+		return (1);
 	return (0);
 }
 
-int __file_find(char *f_name, t_glob *g_lst)
+int	__file_find(char *f_name, t_glob *g_lst)
 {
-	char *tmp;
+	char	*tmp;
 
 	if (!g_lst)
 		return (1);
-	if(!g_lst->to_find || !g_lst->to_find[0])
-		return(__file_find(f_name,g_lst->next));
+	if (!g_lst->to_find || !g_lst->to_find[0])
+		return (__file_find(f_name, g_lst->next));
 	if (g_lst->type == 0)
 	{
-		if(!__strncmp(f_name, g_lst->to_find, __strlen(g_lst->to_find)))
-			return(__file_find(f_name + __strlen(g_lst->to_find), g_lst->next));
+		if (!__strncmp(f_name, g_lst->to_find, __strlen(g_lst->to_find)))
+			return (__file_find(f_name + __strlen(g_lst->to_find),
+					g_lst->next));
 		return (0);
 	}
 	if (g_lst->type == 2)
@@ -319,9 +310,9 @@ int __file_find(char *f_name, t_glob *g_lst)
 	else
 	{
 		tmp = __strstr(f_name, g_lst->to_find);
-    	if (!tmp)
+		if (!tmp)
 			return (0);
-   	 	tmp += __strlen(g_lst->to_find);
+		tmp += __strlen(g_lst->to_find);
 		return (__file_find(tmp, g_lst->next));
 	}
 	return (0);
@@ -329,7 +320,7 @@ int __file_find(char *f_name, t_glob *g_lst)
 
 int	__insert_first_token(t_lexing *lexing, char *new_glob_match)
 {
-	char *tmp;
+	char	*tmp;
 
 	tmp = __strdup(new_glob_match);
 	if (!tmp)
@@ -339,10 +330,10 @@ int	__insert_first_token(t_lexing *lexing, char *new_glob_match)
 	return (1);
 }
 
-int __insert_token(t_lexing *lexing,
+int	__insert_token(t_lexing *lexing,
 	char *new_glob_match, int reset, t_lexing *true_end)
 {
-	static int first = 1;
+	static int	first = 1;
 	t_lexing	*new_token;
 
 	if (reset)
@@ -373,13 +364,13 @@ int	__glob_expand_token(t_lexing *lexing,
 	glob_lst = __create_glob_lst(&lexing->token);
 	if (!glob_lst)
 		return (__lstclear(&d_content, free), 0);
-	if(!__quote_removal_glob(glob_lst))
-		return ( __glob_list_clear(glob_lst),__lstclear(&d_content, free), 0);
+	if (!__quote_removal_glob(glob_lst))
+		return (__glob_list_clear(glob_lst), __lstclear(&d_content, free), 0);
 	while (d_content)
 	{
 		if (__file_find((char *)d_content->content, glob_lst)
 			&& !__insert_token(lexing, (char *)d_content->content, 0, s_nxt))
-				return ( __glob_list_clear(glob_lst), 0);
+			return (__glob_list_clear(glob_lst), 0);
 		d_content = d_content->next;
 	}
 	__insert_token(NULL, NULL, 1, NULL);
@@ -387,17 +378,17 @@ int	__glob_expand_token(t_lexing *lexing,
 	return (1);
 }
 
-int    __handle_wildcards(t_lexing *lexing)
+int	__handle_wildcards(t_lexing *lexing)
 {
 	t_list		*dir_content;
 	t_list		*save;
 	t_lexing	*save_next;
 
 	dir_content = NULL;
-    if (!__init_dir_content(&dir_content))
-		return(0);
+	if (!__init_dir_content(&dir_content))
+		return (0);
 	save = dir_content;
-	while(lexing)
+	while (lexing)
 	{
 		save_next = lexing->next;
 		if (__move_to_next_unquoted_char(lexing->token, '*') >= 0
